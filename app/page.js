ab173d0de4e898147e2f0017d12eea5b62a222b5
Mwangiboyle <mwangiboyle4@gmail.com>
2025-02-26 15:19:44 +0300

    first commit !

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Home2 from "./(homes)/home-2/page";
+export const metadata = {
+  title:
+    "Home 1 || Lexend - Connecxite your linkedin connection assistance",
+  description:
+    "Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+};
+export default function HomePage2() {
+  return (
+    <>
+      <Home2 />
+    </>
+  );
+}
---
+++ data/footer.js
@@ -0,0 +1,40 @@
+export const footerLinks = [
+  {
+    links: [
+      { href: "/page-about", label: "About" },
+      { href: "/page-pricing", label: "Pricing" },
+      { href: "/page-features", label: "Features" },
+      { href: "/page-integrations", label: "Integrations" },
+      { href: "/page-career", label: "Career" },
+      { href: "/page-contact-2", label: "Contact v2" },
+    ],
+  },
+  {
+    links: [
+      { href: "/sign-in-2", label: "Request a demo" },,
+      { href: "/sign-in-2", label: "Sign in v2" },
+      { href: "/sign-up", label: "Sign up" },
+      { href: "/sign-up-2", label: "Sign up v2" },
+      { href: "/reset-password", label: "Reset password" },
+      { href: "/reset-password-2", label: "Reset password v2" },
+    ],
+  },
+  {
+    links: [
+      { href: "/blog", label: "Blog" },
+      { href: "/blog-details/1", label: "Blog detail" },
+      { href: "/page-terms", label: "FAQ" },
+      { href: "/page-not-found", label: "404" },
+      { href: "/page-terms", label: "Terms of service" },
+      { href: "/page-privacy", label: "Privacy policy" },
+    ],
+  },
+];
+
+export const socialLinks = [
+  { href: "#", iconClass: "unicon-logo-linkedin" },
+  { href: "#", iconClass: "unicon-logo-facebook" },
+  { href: "#", iconClass: "unicon-logo-x-filled" },
+  { href: "#", iconClass: "unicon-logo-instagram" },
+  { href: "#", iconClass: "unicon-logo-youtube" },
+];
